67e9e80075d6c7c72d87b74c7f6b3dfbc890c626
HarrisonBoland <harrisonheke@gmail.com>
2022-12-15 21:10:50 +1300

    Add tile map and pacman with movement

--- src/TileMap.js
+++ src/TileMap.js
@@ -1,7 +1,95 @@
-export default class TileMap {
-  constructor() {}
+import Pacman from './Pacman.js';
 
-  draw() {
-    console.log('draw');
+export default class TileMap {
+  constructor(tileSize) {
+    this.tileSize = tileSize;
+
+    this.yellowDot = new Image();
+    this.yellowDot.src = '../images/yellowDot.png';
+
+    this.wall = new Image();
+    this.wall.src = '../images/wall.png';
+  }
+
+  // 0 - dots
+  // 1 - wall
+  // 4 - Pac-Man
+  map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ];
+
+  draw(ctx) {
+    for (let row = 0; row < this.map.length; row++) {
+      for (let column = 0; column < this.map[row].length; column++) {
+        let tile = this.map[row][column];
+        if (tile === 1) {
+          this.#drawWall(ctx, column, row, this.tileSize);
+        } else if (tile === 0) {
+          this.#drawDot(ctx, column, row, this.tileSize);
+        }
+
+        // Shows tile map comment out for production
+        ctx.strokeStyle = 'yellow';
+        ctx.strokeRect(
+          column * this.tileSize,
+          row * this.tileSize,
+          this.tileSize,
+          this.tileSize
+        );
+      }
+    }
+  }
+
+  #drawWall(ctx, column, row, size) {
+    ctx.drawImage(
+      this.wall,
+      column * this.tileSize,
+      row * this.tileSize,
+      size,
+      size
+    );
+  }
+
+  #drawDot(ctx, column, row, size) {
+    ctx.drawImage(
+      this.yellowDot,
+      column * this.tileSize,
+      row * this.tileSize,
+      size,
+      size
+    );
+  }
+
+  getPacman(velocity) {
+    for (let row = 0; row < this.map.length; row++) {
+      for (let column = 0; column < this.map[row].length; column++) {
+        let tile = this.map[row][column];
+        if (tile === 4) {
+          this.map[row][column] = 0;
+          return new Pacman(
+            column * this.tileSize,
+            row * this.tileSize,
+            this.tileSize,
+            velocity,
+            this
+          );
+        }
+      }
+    }
+  }
+
+  setCanvasSize(canvas) {
+    canvas.width = this.map[0].length * this.tileSize;
+    canvas.height = this.map.length * this.tileSize;
   }
 }
